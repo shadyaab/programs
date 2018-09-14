ea6886b58a52782499a479656ddfc593e904c7c7
Shadyaab Akhtar <shadyaab.akhtar@lowes.com>
2018-09-14 12:23:23 +0530

    Java 8 topic included

---
+++ program/src/main/java/net/sady/java8/BufferedReaderAsFunctionalProgramming.java
@@ -0,0 +1,34 @@
+package net.sady.java8;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class BufferedReaderAsFunctionalProgramming {
+	
+	private static final String filePath = "C:\\Users\\sakhtar\\git\\programs\\program\\src\\main\\java\\net\\sady\\java8\\data.txt";
+	
+	public static void main(String[] args) throws FileNotFoundException, IOException {
+		//System.out.println(processFile());
+		System.out.println(processFile((BufferedReader br) -> br.readLine()));
+		System.out.println(processFile((BufferedReader br) -> br.readLine() + br.readLine()));
+
+	}
+	
+	public static String processFile(BufferedReaderProcessor p) throws FileNotFoundException, IOException{
+		try(BufferedReader br = new BufferedReader(new FileReader(filePath))){
+			//return br.readLine();
+			return p.process(br);
+		}
+	}
+	
+	interface BufferedReaderProcessor{
+		String process(BufferedReader br) throws FileNotFoundException, IOException;
+	}
+}
+
+
+
+
+
