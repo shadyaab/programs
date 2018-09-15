3faf1371571087ea1948c35b079b36d6f0157872
Shadyaab Akhtar <shadyaab543@gmail.com>
2018-09-15 14:58:35 +0530

    Stream concept

---
+++ program/src/main/java/net/sady/java8/combinedconcept/CombinedConcept.java
@@ -0,0 +1,101 @@
+package net.sady.java8.combinedconcept;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+public class CombinedConcept {
+	
+	public static void main(String[] args) {
+		Trader raoul = new Trader("Raoul", "Cambridge");
+		Trader mario = new Trader("Mario", "Milan");
+		Trader alan = new Trader("Alan", "Cambridge");
+		Trader brian = new Trader("Brian", "Cambridge");
+		
+		List<Transaction> transactions = Arrays.asList(
+				new Transaction(brian, 2011, 300),
+				new Transaction(raoul, 2012, 1000),
+				new Transaction(raoul, 2011, 400),
+				new Transaction(mario, 2012, 710), 
+				new Transaction(mario, 2012, 700), 
+				new Transaction(alan, 2012, 950)
+				);
+		findSortedTransactionIn2011(transactions);
+		findUniqueCity(transactions);
+		findUniqueCityUsingSet(transactions);
+		findSortedCambridgeTraders(transactions);
+		allTraderSorted(transactions);
+		isTraderInMilan(transactions);
+		transactionFromCambridge(transactions);
+		highestTransaction(transactions);
+	}
+
+	private static void findSortedTransactionIn2011(List<Transaction> transactions) {
+		List<Transaction> list = transactions.stream()
+			.filter(t -> t.getYear() == 2011)
+			//.sorted(comparing(Transaction::getValue))
+			//.sorted((a,b) -> a.getValue() > b.getValue() ? 1)
+			.collect(Collectors.toList());
+		
+		System.out.println(list);
+	}
+	private static void findUniqueCity(List<Transaction> transactions) {
+		List<String> list = transactions.stream()
+				.map(t -> t.getTrader().getCity())
+				.distinct()
+				.collect(Collectors.toList());
+		
+		System.out.println(list);
+	}
+	private static void findUniqueCityUsingSet(List<Transaction> transactions) {
+		Set<String> set = transactions.stream()
+				.map(t -> t.getTrader().getCity())
+				.collect(Collectors.toSet());
+		
+		System.out.println(set);
+	}
+	private static void findSortedCambridgeTraders(List<Transaction> transactions) {
+		List<String> list = transactions.stream()
+				.filter(t -> t.getTrader().getCity().equals("Cambridge"))
+				.map(t -> t.getTrader().getName())
+				.distinct()
+				.sorted((a,b) -> a.compareTo(b))
+				.collect(Collectors.toList());
+		
+		System.out.println(list);
+	}
+	private static void allTraderSorted(List<Transaction> transactions) {
+		String s = transactions.stream()
+				.map(t -> t.getTrader().getName())
+				.distinct()
+				.sorted()
+				.reduce("", (s1,s2) -> s1 + s2);
+				//.collect(joining());
+		
+		System.out.println(s);
+	}
+	
+	private static void isTraderInMilan(List<Transaction> transactions) {
+		Boolean isTraderInMilan = transactions.stream()
+				.anyMatch(t-> t.getTrader().getCity().equals("Milan"));
+		
+		System.out.println(isTraderInMilan);
+	}
+	
+	private static void transactionFromCambridge(List<Transaction> transactions) {
+		transactions.stream()
+			.filter(t -> t.getTrader().getCity().equals("Cambridge"))
+			.map(Transaction::getValue)
+			.forEach(System.out::println);
+	
+	}
+	private static void highestTransaction(List<Transaction> transactions) {
+		Optional<Integer> opt = transactions.stream()
+			//.filter(t -> t.getValue() < 500)
+			.map(Transaction::getValue)
+			.reduce(Integer::max);
+		System.out.println(opt.get());
+	}
+}
---
+++ program/src/main/java/net/sady/java8/reduce/Basic.java
@@ -0,0 +1,59 @@
+package net.sady.java8.reduce;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
+
+import net.sady.java8.stream.Dish;
+
+public class Basic {
+	
+	public static void main(String[] args) {
+	
+		
+		List<Dish> menu = Arrays.asList(
+				new Dish("pork", false, 800, Dish.Type.MEAT),
+				new Dish("beef", false, 700, Dish.Type.MEAT),
+				new Dish("chicken", false, 400, Dish.Type.MEAT),
+				new Dish("french fries", true, 530, Dish.Type.OTHER),
+				new Dish("rice", true, 350, Dish.Type.OTHER),
+				new Dish("season fruit", true, 120, Dish.Type.OTHER),
+				new Dish("pizza", true, 550, Dish.Type.OTHER),
+				new Dish("prawns", false, 300, Dish.Type.FISH),
+				new Dish("salmon", false, 450, Dish.Type.FISH) );
+		
+		countElements(menu);
+		
+		sumCalories(menu);
+		
+		maxCalory(menu);
+		
+		 
+	}
+
+	private static void countElements(List<Dish> menu) {
+		int count = menu.stream()
+			 	.map(d -> 1)
+				.reduce(0,(a,b) -> a+b);
+		
+		System.out.println(count);
+	}
+	
+	private static void sumCalories(List<Dish> menu) {
+		int sum =  menu.stream()
+			 	.map(d -> d.getCalories())
+				.reduce(0,(a,b) -> a+b);
+	 
+		System.out.println(sum);
+	}
+	
+	private static void maxCalory(List<Dish> menu) {
+		int max =  menu.stream()
+			 	.map(d -> d.getCalories())
+				.reduce(0, (a,b) -> a>b?a:b);
+	 
+		System.out.println(max);
+	}
+
+}
